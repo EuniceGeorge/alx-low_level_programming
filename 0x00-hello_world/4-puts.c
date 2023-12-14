aeb09ddc47aa2a288332a29f8d7368640d3506df
Eunice George <georgeunice408@gmail.com>
2023-12-14 11:56:31 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+ * main - return main function
+ * Return: 0 on success
+ */
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	getchar();
+	return (0);
+}
