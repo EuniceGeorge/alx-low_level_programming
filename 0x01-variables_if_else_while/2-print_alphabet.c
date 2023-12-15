f45a3223b7d53e2f0f0a373fa7c58df1e0a82bf9
Eunice George <georgeunice408@gmail.com>
2023-12-15 15:44:16 -0800

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- The main function
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
