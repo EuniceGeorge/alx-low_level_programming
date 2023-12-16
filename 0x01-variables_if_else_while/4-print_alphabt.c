5fbef5666b58a7a862b7cf2bc22d804dbd7d0b0f
Eunice George <georgeunice408@gmail.com>
2023-12-15 16:12:45 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- This is the main function
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
