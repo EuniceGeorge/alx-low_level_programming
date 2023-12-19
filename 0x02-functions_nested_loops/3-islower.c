ae1ed6db08e8f79e14bc5b2666c97fd51d01d34e
Eunice George <georgeunice408@gmail.com>
2023-12-19 13:20:41 -0800

    print character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,16 +9,12 @@
 
 int _islower(int c)
 {
-	for (c = 97; c <= 122; c++)
+	if (c <= 97 || c > 122)
 	{
-		if (c <= 97 || c > 122)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
 	}
-	return (c);
+	else
+	{
+		return (0);
+	}
 }
