b6a83700fc37a14c990207bf2fb871920dad8249
Eunice George <georgeunice408@gmail.com>
2023-12-19 12:18:38 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main- This is the main function
+ * Description: a program that prints _putchar
+ * Return: 0 on success
+ *
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
