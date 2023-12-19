1d1873b2f5ef04c7b671af354452a73ef01c1a67
Eunice George <georgeunice408@gmail.com>
2023-12-19 14:03:26 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs- The entry point
+ *@n: the value to te
+ *Return: the absolute value
+ */
+
+int _abs(int n)
+{
+	int abs_value;
+
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
+
