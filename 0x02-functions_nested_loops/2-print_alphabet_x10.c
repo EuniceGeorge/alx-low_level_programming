404ebcd789719a040899da9e29ff0762b2633f4b
Eunice George <georgeunice408@gmail.com>
2023-12-19 12:46:50 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- This is the main entry
+ * Description: prints lowercase alphabet 10x
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10;; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
