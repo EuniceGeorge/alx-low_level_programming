b6045fd3d14a137f5dbe05eae2b1a52812cf4d42
Eunice George <georgeunice408@gmail.com>
2023-12-19 13:07:09 -0800

    print alphabte

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _islower- Entry of the function
+ * Description: prints only lowercase
+ * @c: The parameter
+ * Return: 1 on success and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	for (c = 97; c <= 122 c++)
+	{
+		if (c <= 97 || c > 122)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
