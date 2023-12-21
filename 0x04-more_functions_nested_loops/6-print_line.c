659dcba8b5ddcba6715a3b6a705fa85132564a3c
Eunice George <georgeunice408@gmail.com>
2023-12-21 15:47:39 -0800

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line- This is the entry point
+ * Decsription: print a -
+ * @n: the number of line to prints
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int size;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (size = 0; size <= n; size++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
