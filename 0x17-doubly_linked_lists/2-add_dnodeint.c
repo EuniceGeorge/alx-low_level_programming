f3f67f83da5d6a26ab4ed966f985cbadc9fe1a59
Eunice George <georgeunice408@gmail.com>
2024-03-22 21:11:18 -0700

    task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,21 +1,38 @@
 #include "lists.h"
+
 /**
-  * add_dnodeint - Adds a node to the beginning of a doubly linked list
-  * @head: head of the linked list
-  * @n: value to add to the list
-  * Return: pointer to the element added
-  */
+ * add_dnodeint - function to add node to the beginning of a dlistint_t list
+ * @head: input pointer to head of dlistint_t list
+ * @n: input data to insert in new node
+ * Return: pointer to newly added element or NULL if failed
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *tmp;
+	dlistint_t *newnode;
 
-	tmp = *head;
-	tmp = malloc(sizeof(dlistint_t));
-	if (tmp == NULL)
+	/* malloc out size of newnode and, if successful, set as single node */
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
 		return (NULL);
-	tmp->n = n;
-	tmp->prev = NULL;
-	tmp->next = *head;
-	*head = tmp;
-	return (tmp);
+	newnode->n = n;
+	newnode->prev = NULL;
+	newnode->next = NULL;
+
+	/* check for no list (head == NULL) & set newnode to be only element */
+	if (head == NULL)
+	{
+		head = &newnode;
+		return (newnode);
+	}
+	/* check for empty list (*head == NULL) & set newnode to only element */
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+	/* set newnode to point to start of list & head to point to newnode */
+	newnode->next = *head;
+	(*head)->prev = newnode;
+	*head = newnode;
+	return (newnode);
 }
