0b7799f0b752df949d3668177652b6ee4a91be87
Eunice George <georgeunice408@gmail.com>
2024-04-23 09:37:22 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -44,7 +44,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		newnode->next = ht->array[index];
 		ht->array[index] = newnode;
 	}
-	free(newnode->next);
-	free(newnode);
 	return (1);
 }
