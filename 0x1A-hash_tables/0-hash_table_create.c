a7582f989c7128aa17a685307dd8bff50c0ae39d
Eunice George <georgeunice408@gmail.com>
2024-04-23 02:20:47 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,24 +5,26 @@
   * Return: pointer to the hash table
   */
 
-hash_table_t *hash_table_create(unsigned long int size){
-	
+hash_table_t *hash_table_create(unsigned long int size)
+{
 	unsigned long int i;
 	hash_table_t *newtable;
 
 	newtable = malloc(sizeof(hash_table_t));
 	newtable->size = size;
 
-	if(size <= 0)
+	if (size <= 0)
 		return (NULL);
-	newtable->array = malloc(sizeof(hash_table_t)*size);
+	newtable->array = malloc(sizeof(hash_table_t) * size);
 	if (!newtable->array)
 	{
 		free(newtable);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++){
+
+	for (i = 0; i < size; i++)
+	{
 		newtable->array[i] = NULL;
 	}
-	return newtable;
+	return (newtable);
 }
