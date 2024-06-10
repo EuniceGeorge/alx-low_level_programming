28463313ff762156774d7422d080baecfa4cf20d
EuniceGeorge <georgeunice408.com>
2024-06-10 11:22:04 +0100

    task o

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets a value
+ *
+ * @n: The integer to change
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
